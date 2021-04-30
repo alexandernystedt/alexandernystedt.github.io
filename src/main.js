90506bea7818bce0b3176d14e52cc5082af09e7f
Alexander <45545021+alexandernystedt@users.noreply.github.com>
2021-04-30 10:36:40 +0200

    mps to kmph

--- src/main.js
+++ src/main.js
@@ -201,7 +201,7 @@ class Car
         this.vel_y = forward_vel_y + right_vel_y * this.drift;
         
         let vel_magnitude = Math.sqrt(Math.abs(this.vel_x * this.vel_x + this.vel_y * this.vel_y));
-        speedometer.innerHTML = vel_magnitude + " kmph";
+        speedometer.innerHTML = vel_magnitude * 3.6 + " kmph";
         
         this.position.x += this.vel_x;
         this.position.z += this.vel_y;
